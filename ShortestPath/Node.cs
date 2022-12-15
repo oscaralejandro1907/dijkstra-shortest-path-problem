2a0efc8142801d4df400dd0703916fbcf020ed09
Oscar Hernandez <oscar.hernandez@aleph5.com>
2022-12-15 10:54:16 -0600

    update class Node

--- ShortestPath/Node.cs
+++ ShortestPath/Node.cs
@@ -8,5 +8,15 @@ namespace ShortestPath
 {
     internal class Node
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Node predecessorNode { get; set; }
+        public List<Node> ListAdjacentNodes { get; set; }
+
+        public Node(int id)
+        {
+            Id = id;
+            ListAdjacentNodes = new List<Node>();
+        }
     }
 }
